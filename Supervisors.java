adc82ca04458c92afd197978e36a9ef7729e26a7
josh3311 <joshuafowah0@gmail.com>
2025-03-08 00:31:21 -0500

    Update Supervisors.java
    "Done with employee, consultant, and supervisor class; main class left to work. *Thinking of using an ArrayList in the main..."

--- Supervisors.java
+++ Supervisors.java
@@ -1,27 +1,18 @@
 package PaymentSystem;
 
-public class Supervisors extends Millers implements  {
-	/**
-	This code should contain your Supervisor class declaration, constructor and method. 
-	A Supervisor is an employee who also receives a bonus. 
-	You must implement the bi-weekly pay for the employee according to instructions provided.
-	*/
-
-	// TO DO: TASK 1 - YOUR CODE FOR THE CLASS GOES HERE
-	// Declare the class and any variable
-
-	/**
-	    TO DO: TASK 2 - Constructs a supervisor with a given name, annual pay and bi-weekly bonus.
-	   @param name the name of this employee
-	   @param pay the annual pay
-	   @param bonus the bi-weekly bonus
-	 */
-	// YOUR Constructor code goes here!!!
-
-
-	// TO DO: TASK 3 - YOUR CODE FOR COMPUTING THE BI-WEEKLY PAY FOR THIS CATEGORY OF MILLER GOES HERE.
-
-	// Hint: Note the relationship between this class and any other class. How will you apply that to your code?
+public class Supervisors extends Employees {
+	
+	double bonusSupervisor;
+	
+	public Supervisors(String name, double pay,double bonus) {
+		super(name, pay); //calling constructor of super class, avoiding redundancy.
+		this.bonusSupervisor = bonus;
+	}
+	
+	@Override
+	public double biWeeklyPay(int hoursWorked) {
+		return super.biWeeklyPay(hoursWorked) + bonusSupervisor;
+	}
 	
 }
 
